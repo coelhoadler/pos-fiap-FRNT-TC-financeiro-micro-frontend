58615e6e7490626ee584270ef29f3301ec5c9c2c
Adler Santos <adlercoelhosantos12@gmail.com>
2025-06-24 20:56:31 -0300

    feat: integrate Tailwind CSS and PostCSS into login and dashboard apps
    - Updated package.json in the login app to include Tailwind CSS, PostCSS, and related dependencies.
    - Added PostCSS configuration files for both login and dashboard apps.
    - Created index.css files in both apps to import Tailwind CSS.
    - Modified root component in the login app to apply Tailwind CSS classes.
    - Updated webpack configuration in the login app to handle CSS files with PostCSS.
    - Added Tailwind CSS configuration file for the login app.

--- apps/dashboard/webpack.config.js
+++ apps/dashboard/webpack.config.js
@@ -11,6 +11,15 @@ module.exports = (webpackConfigEnv, argv) => {
   });
 
   return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            "postcss-loader",
+          ],
+        },
+      ]
+    },    
   });
 };
