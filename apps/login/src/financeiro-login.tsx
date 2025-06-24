91474dacf9e64d36a99f28de456f0a5de528e00e
Adler Santos <adlercoelhosantos12@gmail.com>
2025-06-23 21:00:35 -0300

    refactor: remove husky installation script and update root component imports

--- apps/login/src/financeiro-login.tsx
+++ apps/login/src/financeiro-login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import singleSpaReact from "single-spa-react";
-import Root from "./root.component";
+import Root from "./root";
 
 const lifecycles = singleSpaReact({
   React,
