244bc72c936ee6e16afb55d7336d4cd476c5507e
Erick <erick1661@gmail.com>
2025-07-09 15:21:22 -0300

    ajustes header

--- apps/login/src/components/Header/index.tsx
+++ apps/login/src/components/Header/index.tsx
@@ -5,7 +5,6 @@ import closeIcon from "./../../assets/close-icon.svg";
 import hamburgerMenuIcon from "./../../assets/hamburger-menu-icon.svg";
 import Button from "../Button";
 import { TMenuLinksItems, TMenuMobile } from "../../types/TMenu";
-import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
 
 const MenulinksItems: TMenuLinksItems[] = [
   {
@@ -40,63 +39,7 @@ const MenuMobile = ({ className }: TMenuMobile) => {
     <div
       className={`max-md:block hidden text-white ${className ? className : ""}`}
     >
-      <Disclosure as="nav" className="md:hidden flex items-center">
-        {({ open, close }) => (
-          <>
-            <DisclosureButton className="text-gray-700 hover:text-black focus:outline-none">
-              {open ? (
-                ""
-              ) : (
-                <img
-                  width={30}
-                  height={30}
-                  src={hamburgerMenuIcon}
-                  alt={"Abrir Menu"}
-                />
-              )}
-            </DisclosureButton>
-            <div
-              className={`transition-all duration-500 fixed w-full top-0 h-screen z-30 bg-[#E4EDE3] p-4 ${
-                open ? "right-0" : "right-[100%]"
-              }`}
-            >
-              <DisclosurePanel className="flex flex-col px-4 pb-4 space-y-2 items-end mt-4">
-                <DisclosureButton className="text-gray-700 hover:text-black focus:outline-none">
-                  {open ? (
-                    <img
-                      width={24}
-                      height={24}
-                      src={closeIcon}
-                      alt={"Fechar Menu"}
-                    />
-                  ) : (
-                    ""
-                  )}
-                </DisclosureButton>
-                <div className="flex flex-col items-center justify-center w-full mt-6">
-                  <button
-                    onClick={() => close()}
-                    className="text-white"
-                    type="button"
-                  >
-                    <img src={closeIcon} alt={"Fechar Menu"} />
-                  </button>
-                  <nav className="space-y-4 text-white">
-                    {MenulinksItems.map((link) => (
-                      <LinkItem
-                        key={link.text}
-                        text={link.text}
-                        href={link.href}
-                        isBlank={link.isBlank}
-                      />
-                    ))}
-                  </nav>
-                </div>
-              </DisclosurePanel>
-            </div>
-          </>
-        )}
-      </Disclosure>
+      menu mobile
     </div>
   );
 };
