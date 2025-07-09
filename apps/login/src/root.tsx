2aa11a8863e95327b7dc47a640449e037d8371d0
Erick <erick1661@gmail.com>
2025-07-08 22:24:34 -0300

    rename file header, footer e home

--- apps/login/src/root.tsx
+++ apps/login/src/root.tsx
@@ -1,7 +1,7 @@
-import Home from "./components/Home/home";
+import Home from "./components/Home";
 import "./index.css";
-import Header from "./components/Header/header";
-import Footer from "./components/Footer/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Root(props) {
   return (
