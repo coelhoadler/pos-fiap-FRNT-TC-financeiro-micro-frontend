a503a5e55f307ddd6fcade87f9ac9fcf39f64897
Adler Santos <adlercoelhosantos12@gmail.com>
2025-07-23 22:05:27 -0300

    update environment variables for MongoDB connection and add JWT and BCRYPT configurations

--- apps/api/database/dbConnection.js
+++ apps/api/database/dbConnection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 exports.mongooseConnection = () => {
-  const url = `mongodb://admin:admin123@mongodb:27017/bytebank?authSource=admin`;
+
+  const mongoURI = process.env.MONGO_URI || 'mongodb://admin:admin123@localhost:27017/bytebank?authSource=admin';
 
   mongoose.Promise = global.Promise;
 
-  return mongoose.connect(url);
+  return mongoose.connect(mongoURI);
 };
