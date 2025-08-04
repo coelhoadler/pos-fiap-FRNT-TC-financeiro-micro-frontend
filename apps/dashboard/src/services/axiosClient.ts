c1c99a0a26bd5bfad5a0a069e9616ba6ee8df281
Adler Santos <adlercoelhosantos12@gmail.com>
2025-08-04 20:06:29 -0300

    fix(axios): setando token no headers

--- apps/dashboard/src/services/axiosClient.ts
+++ apps/dashboard/src/services/axiosClient.ts
@@ -4,6 +4,7 @@ const axiosClient = axios.create({
   baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json',
+    'Authorization': `Bearer ${localStorage.getItem('token') || ''}`,
   },
   withCredentials: true,
 });
