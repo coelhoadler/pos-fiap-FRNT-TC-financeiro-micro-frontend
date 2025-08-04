1edebaac5bd549d6f84310250e607fc24f1c0bd8
Adler Santos <adlercoelhosantos12@gmail.com>
2025-08-04 13:18:26 -0300

    refac(useUserInfos): verificando objeto de user no localStorage

--- apps/login/src/hooks/useUserInfos.tsx
+++ apps/login/src/hooks/useUserInfos.tsx
@@ -1,41 +1,8 @@
-
-import { useState, useEffect } from "react";
-import { userInfos } from "../services/userService";
-import { UserInfo } from "../interfaces/IUser";
-
 function useUserInfo() {
-  const [user, setUser] = useState<UserInfo | null>(null);
+  const userLocalStorage = localStorage.getItem('user');
+  const userInfo = userLocalStorage ? JSON.parse(userLocalStorage) : {};
 
-  useEffect(() => {
-    const getUserData = async () => {
-
-      try {
-        const data = await userInfos();
-
-        if (data.messageError) {
-          setUser(null)
-        }
-
-        if (!data.messageError) {
-          setUser(data);
-          console.log("Dados do usuário:", data);
-        }
-      } catch (error) {
-        setUser(null);
-        if (error?.status === 401) {
-          window.location.href = '/login';
-        }
-        console.error("Erro ao buscar informações do usuário getUserData:", error);
-      }
-
-    };
-
-
-
-    getUserData();
-  }, []);
-
-  return { user };
+  return userInfo;
 }
 
 export default useUserInfo;
