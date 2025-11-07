d035cb33104eef40dbdd1f7245f66106c869043e
Erick <erick1661@gmail.com>
2025-11-07 16:38:05 -0300

    exportando input, label e messagem

--- packages/ui/src/financeiro-ui.ts
+++ packages/ui/src/financeiro-ui.ts
@@ -6,5 +6,9 @@ export { Button } from './components/generic/button';
 export { Header } from './components/dashboard/Header';
 export { CustomModal } from './components/generic/customModal';
 
+export { Input } from './components/generic/input';
+export { Label } from './components/generic/label';
+export { Message } from './components/generic/messages';
+
 export { DesktopMenu, MenuItens, MobileMenu } from './components/dashboard/MobileMenu';
 export { Title } from './components/dashboard/Title';
